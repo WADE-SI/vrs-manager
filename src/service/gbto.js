f5681ce3a563c278556ca9fd4d5962e41967eebb
Minjun <84955607+dev-yuka05@users.noreply.github.com>
2023-10-05 14:51:01 +0900

    Delete src directory

--- src/service/gbto.js
+++
@@ -1,8 +0,0 @@
-export function atGigabytes(sizeInGB) {
-    if (sizeInGB >= 1024) {
-      const terabytes = sizeInGB / 1024;
-      return `${terabytes.toFixed(1)} TB`;
-    } else {
-      return sizeInGB.toFixed(1) + 'GB';
-    }
-}
